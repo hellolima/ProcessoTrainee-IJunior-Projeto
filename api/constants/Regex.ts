31a722c11641f6288f817d3faa17d43100f88182
Gabriella <gabrielladelimadev@gmail.com>
2023-04-29 17:51:38 -0300

    fix: exportando arquivo jeito correto

--- api/constants/Regex.ts
+++ api/constants/Regex.ts
@@ -1,4 +1,4 @@
-const Regex = {
+export const Regex = {
     FOTO: /^https?:\/\/\S+\.(jpg|png|gif)$/i,
     CATEGORIA: /^[a-zA-ZÀ-ÿ -]*$/,
     NACIONALIDADE: /^[a-zA-Z]+$/,
@@ -6,5 +6,3 @@ const Regex = {
     NOME: /^[a-zA-Z0-9]{1,12}$/,
     SENHA: /^.{1,20}$/
 };
-
-module.exports = Regex;
