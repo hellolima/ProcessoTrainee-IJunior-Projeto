a23bbb97ede6aa726f244bdfb6ae56e6d808936b
Gabriella <gabrielladelimadev@gmail.com>
2023-04-29 17:09:08 -0300

    feature: adicionando global.d.ts

---
+++ api/@types/global.d.ts
@@ -0,0 +1,30 @@
+
+interface PayloadParams {
+    id: string;
+    name: string;
+    email: string;
+    role: string;
+}
+
+interface Env {
+	DB: string;
+	DB_USER: string;
+	DB_PASSWORD: string;
+	DB_HOST: string;
+	SECRET_KEY: string;
+	NODE_ENV: string;
+	JWT_EXPIRATION: string;
+	APP_URL: string;
+} 
+
+declare global {
+    namespace Express {
+        interface Request{
+            user?: PayloadParams;  
+        }
+    };
+
+    namespace NodeJS {
+        interface ProcessEnv extends Env {};
+    };
+}
